88dabb2d9b1d3a0969c44d7263f19a16fac072bc
demir <odemirci@na.edu>
2020-02-03 22:12:57 -0500

    First Codes

---
+++ src/Secondpackage/LogicalOperators.java
@@ -0,0 +1,24 @@
+package Secondpackage;
+
+public class LogicalOperators {
+    public static void main(String args[]){
+        /* && = logical "and"
+         || = "or"
+         ! = "not"
+         == --> "equal"
+         */
+if(1==0){
+    System.out.println("Yes");}
+char a = 'a';
+char b = 'b';
+if(b>a){
+    System.out.println("yes");
+    System.out.println("lin2");
+}
+int num = 5;
+if(num%2==1){
+    System.out.println("odd");
+}
+
+    }
+}
