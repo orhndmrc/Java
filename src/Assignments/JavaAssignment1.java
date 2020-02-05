111594817c1ea9769caea16077192fa0d1bd1427
demir <odemirci@na.edu>
2020-02-04 22:17:10 -0500

    First java assignment

---
+++ src/Assignments/JavaAssignment1.java
@@ -0,0 +1,35 @@
+package Assignments;
+import java.util.Scanner;
+public class JavaAssignment1 {
+    public static void main(String[] args) {
+        Scanner MyKeyword = new Scanner(System.in);
+        int id1;  int id2; int id3;
+        int q1;    int q2;  int q3;
+        double p1; double p2; double p3;
+        System.out.println("Enter item1 id number:");
+        id1 = MyKeyword.nextInt();
+        System.out.println("Enter item1 quantity:");
+        q1 = MyKeyword.nextInt();
+        System.out.println("Enter item1 price:");
+        p1 = MyKeyword.nextDouble();
+        System.out.println("Enter item2 id number:");
+        id2 = MyKeyword.nextInt();
+        System.out.println("Enter item2 quantity:");
+        q2 = MyKeyword.nextInt();
+        System.out.println("Enter item2 price:");
+        p2 = MyKeyword.nextDouble();
+        System.out.println("Enter item3 id number:");
+        id3 = MyKeyword.nextInt();
+        System.out.println("Enter item3 quantity:");
+        q3 = MyKeyword.nextInt();
+        System.out.println("Enter item3 price:");
+        p3 = MyKeyword.nextDouble();
+        System.out.println("The receipt");
+        System.out.println("Item id\t\tquantity\t\tprice");
+        System.out.println("----------------------------------");
+        System.out.println(id1+"\t\t\t\t"+q1+"\t\t\t"+p1);
+        System.out.println(id2+"\t\t\t\t"+q2+"\t\t\t"+p2);
+        System.out.println(id3+"\t\t\t\t"+q3+"\t\t\t"+p3);
+        System.out.format("TOTAL\t\t\t"+(q1+q2+q3)+"\t\t\t"+(p1+p2+p3));
+    }
+}
