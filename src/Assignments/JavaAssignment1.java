0d7d0e14486f1d781d5b9ab60848f8052200c96c
demir <odemirci@na.edu>
2020-02-05 23:53:35 -0500

    First java assignment(updated)

--- src/Assignments/JavaAssignment1.java
+++ src/Assignments/JavaAssignment1.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class JavaAssignment1 {
     public static void main(String[] args) {
         Scanner MyKeyword = new Scanner(System.in);
-        int id1;  int id2; int id3;
-        int q1;    int q2;  int q3;
-        double p1; double p2; double p3;
+        int id1;  int id2; int id3;  //id1 = first item's id, id2= second item's id
+        int q1;    int q2;  int q3; //q1 = first item's quantity
+        double p1; double p2; double p3; // p1 = first item's price
         System.out.println("Enter item1 id number:");
         id1 = MyKeyword.nextInt();
         System.out.println("Enter item1 quantity:");
@@ -27,9 +27,9 @@ public class JavaAssignment1 {
         System.out.println("The receipt");
         System.out.println("Item id\t\tquantity\t\tprice");
         System.out.println("----------------------------------");
-        System.out.println(id1+"\t\t\t\t"+q1+"\t\t\t"+p1);
-        System.out.println(id2+"\t\t\t\t"+q2+"\t\t\t"+p2);
-        System.out.println(id3+"\t\t\t\t"+q3+"\t\t\t"+p3);
-        System.out.format("TOTAL\t\t\t"+(q1+q2+q3)+"\t\t\t"+(p1+p2+p3));
+        System.out.println(id1+"\t\t\t\t"+q1+"\t\t\t$"+q1*p1);
+        System.out.println(id2+"\t\t\t\t"+q2+"\t\t\t$"+q2*p2);
+        System.out.println(id3+"\t\t\t\t"+q3+"\t\t\t$"+q3*p3);
+        System.out.format("TOTAL\t\t\t"+(q1+q2+q3)+"\t\t\t$"+((q1*p1)+(q2*p2)+(q3*p3)));
     }
 }
