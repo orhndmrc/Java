66de1a6b25eb6bff100d69d69af48d4e7c241e0c
demir <odemirci@na.edu>
2020-02-08 18:22:55 -0500

    Feb 8th

---
+++ src/Feb_eight/StringComparisonExample.java
@@ -0,0 +1,11 @@
+package Feb_eight;
+
+public class StringComparisonExample {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "a";
+        if (a.equals(b)) {
+            System.out.println("Hello");
+        }
+    }
+}
